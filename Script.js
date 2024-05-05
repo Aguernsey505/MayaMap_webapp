ffc0551076633a5b56d80e56f7907d97d49a2bb0
Adam Guernsey <Aguernsey505@gmail.com>
2024-05-04 21:42:36 -0600

    Finishing touches

--- Script.js
+++ Script.js
@@ -1,18 +1,5 @@
-// Add console.log to check to see if our code is working.
-console.log("working");
-
-
 //create map
 var map = L.map('map').setView[17.195, -88.687],
-
-const map = L.map('map').setView([17.195, -88.687], 8);
-
-const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 10,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-
-
 
 //create tile layer and add basemap
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -23,6 +10,37 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 
 
+// Load GeoJSON file
+fetch('maya_sites.geojson')
+  .then(response => response.json())
+  .then(data => {
+    // Load CSV file
+    d3.csv('radiocarbon1.csv').then(csvData => {
+      // Create an object to store the radiocarbon data
+      var radiocarbonData = {};
+      csvData.forEach(row => {
+        radiocarbonData[row.site_name] = {
+          num_of_dates: row.num_of_dates,
+          date_range: row.date_range,
+          time_period: row.time_period
+        };
+      });
+
+      // Create GeoJSON layer with popups
+      L.geoJSON(data, {
+        onEachFeature: function(feature, layer) {
+          var siteData = radiocarbonData[feature.properties.name];
+          layer.bindPopup(`
+            <strong>${feature.properties.name}</strong><br>
+            Occupation: ${siteData.time_period}<br>
+            Radiocarbon dates: ${siteData.num_of_dates}<br>
+            Date range: ${siteData.date_range}<br>
+            Description: ${feature.properties.description}
+          `);
+        }
+      }).addTo(map);
+    });
+  });
 
 
 /* js rewritten to fix map functon
---
+++ mayaSites2.js
@@ -0,0 +1,92 @@
+var mayaSites = {
+
+
+
+
+    "features": [
+
+
+        { "type": "Feature", "properties": { "FID": 515, "AREA": 0, "PERIMETER": 0, "ARCHR_": 409, "ARCHR_ID": 165, "COUNTRY": "BELIZE", "NAME": "Uxbenka", "POINTSOURC": "NAT. GEOG. LAND OF THE MAYAS", "PROVINCE": 16 }, "geometry": { "type": "Point", "coordinates": [-89.099468489747196, 16.245871331638298] } },
+
+        { "type": "Feature", "properties": { "FID": 519, "AREA": 0, "PERIMETER": 0, "ARCHR_": 413, "ARCHR_ID": 162, "COUNTRY": "BELIZE", "NAME": "Wild Cane Cay", "POINTSOURC": "NAT. GEOG. LAND OF THE MAYAS", "PROVINCE": 16 }, "geometry": { "type": "Point", "coordinates": [-88.655748902842106, 16.230470323161299] } },
+
+        { "type": "Feature", "properties": { "FID": 529, "AREA": 0, "PERIMETER": 0, "ARCHR_": 423, "ARCHR_ID": 163, "COUNTRY": "BELIZE", "NAME": "Frenchmans Cay", "POINTSOURC": "NAT. GEOG. LAND OF THE MAYAS", "PROVINCE": 16 }, "geometry": { "type": "Point", "coordinates": [-88.722093373834994, 16.145542809917199] } },
+
+        { "type": "Feature", "properties": { "FID": 511, "AREA": 0, "PERIMETER": 0, "ARCHR_": 405, "ARCHR_ID": 166, "COUNTRY": "BELIZE", "NAME": "Lubaantun", "POINTSOURC": "NAT. GEOG. LAND OF THE MAYAS", "PROVINCE": 16 }, "geometry": { "type": "Point", "coordinates": [-88.910005021498506, 16.3085105868475] } },
+
+        { "type": "Feature", "properties": { "FID": 506, "AREA": 0, "PERIMETER": 0, "ARCHR_": 400, "ARCHR_ID": 167, "COUNTRY": "BELIZE", "NAME": "Nim li punit", "POINTSOURC": "NAT. GEOG. LAND OF THE MAYAS", "PROVINCE": 16 }, "geometry": { "type": "Point", "coordinates": [-88.818392365072398, 16.346990412320899] } },
+
+        { "type": "Feature", "properties": { "FID": 503, "AREA": 0, "PERIMETER": 0, "ARCHR_": 397, "ARCHR_ID": 168, "COUNTRY": "BELIZE", "NAME": "Xnaheb", "POINTSOURC": "NAT. GEOG. LAND OF THE MAYAS", "PROVINCE": 16 }, "geometry": { "type": "Point", "coordinates": [-88.884482028598597, 16.3742531801167] } },
+
+        { "type": "Feature", "properties": { "FID": 448, "AREA": 0, "PERIMETER": 0, "ARCHR_": 354, "ARCHR_ID": 161, "COUNTRY": "BELIZE", "NAME": "False Cay", "POINTSOURC": "NAT. GEOG. LAND OF THE MAYAS", "PROVINCE": 15 }, "geometry": { "type": "Point", "coordinates": [-88.375852704715797, 16.582615160092601] } },
+
+        { "type": "Feature", "properties": { "FID": 438, "AREA": 0, "PERIMETER": 0, "ARCHR_": 196, "ARCHR_ID": 158, "COUNTRY": "BELIZE", "NAME": "Moho Cay", "POINTSOURC": "NAT. GEOG. LAND OF THE MAYAS", "PROVINCE": 11 }, "geometry": { "type": "Point", "coordinates": [-88.181030857994998, 17.537133955819701] } },
+
+        { "type": "Feature", "properties": { "FID": 430, "AREA": 0, "PERIMETER": 0, "ARCHR_": 192, "ARCHR_ID": 178, "COUNTRY": "BELIZE", "NAME": "Chan Chich", "POINTSOURC": "NAT. GEOG. LAND OF THE MAYAS", "PROVINCE": 14 }, "geometry": { "type": "Point", "coordinates": [-89.092228569007602, 17.561584426194599] } },
+
+        { "type": "Feature", "properties": { "FID": 405, "AREA": 0, "PERIMETER": 0, "ARCHR_": 332, "ARCHR_ID": 169, "COUNTRY": "BELIZE", "NAME": "Caracol", "POINTSOURC": "NAT. GEOG. LAND OF THE MAYAS", "PROVINCE": 12 }, "geometry": { "type": "Point", "coordinates": [-89.107525403581604, 16.739387106896402] } },
+
+        { "type": "Feature", "properties": { "FID": 401, "AREA": 0, "PERIMETER": 0, "ARCHR_": 330, "ARCHR_ID": 170, "COUNTRY": "BELIZE", "NAME": "Mountain Cow", "POINTSOURC": "NAT. GEOG. LAND OF THE MAYAS", "PROVINCE": 12 }, "geometry": { "type": "Point", "coordinates": [-89.0445007214215, 16.770116571168401] } },
+
+        { "type": "Feature", "properties": { "FID": 389, "AREA": 0, "PERIMETER": 0, "ARCHR_": 324, "ARCHR_ID": 160, "COUNTRY": "BELIZE", "NAME": "Kendal", "POINTSOURC": "NAT. GEOG. LAND OF THE MAYAS", "PROVINCE": 15 }, "geometry": { "type": "Point", "coordinates": [-88.366459596224999, 16.8315813886916] } },
+
+        { "type": "Feature", "properties": { "FID": 386, "AREA": 0, "PERIMETER": 0, "ARCHR_": 170, "ARCHR_ID": 154, "COUNTRY": "BELIZE", "NAME": "Lamania", "POINTSOURC": "NAT. GEOG. LAND OF THE MAYAS", "PROVINCE": 14 }, "geometry": { "type": "Point", "coordinates": [-88.664366421492403, 17.746786408832701] } },
+
+        { "type": "Feature", "properties": { "FID": 381, "AREA": 0, "PERIMETER": 0, "ARCHR_": 168, "ARCHR_ID": 151, "COUNTRY": "BELIZE", "NAME": "Colha", "POINTSOURC": "NAT. GEOG. LAND OF THE MAYAS", "PROVINCE": 11 }, "geometry": { "type": "Point", "coordinates": [-88.347897466739099, 17.758469449128199] } },
+
+        { "type": "Feature", "properties": { "FID": 369, "AREA": 0, "PERIMETER": 0, "ARCHR_": 163, "ARCHR_ID": 200, "COUNTRY": "BELIZE", "NAME": "Las Milpas", "POINTSOURC": "NAT. GEOG. LAND OF THE MAYAS", "PROVINCE": 14 }, "geometry": { "type": "Point", "coordinates": [-89.037088914768404, 17.797027124882501] } },
+
+        { "type": "Feature", "properties": { "FID": 367, "AREA": 0, "PERIMETER": 0, "ARCHR_": 162, "ARCHR_ID": 155, "COUNTRY": "BELIZE", "NAME": "Las Milpas", "POINTSOURC": "NAT. GEOG. LAND OF THE MAYAS", "PROVINCE": 14 }, "geometry": { "type": "Point", "coordinates": [-89.038999029648096, 17.798153433560699] } },
+
+        { "type": "Feature", "properties": { "FID": 359, "AREA": 0, "PERIMETER": 0, "ARCHR_": 158, "ARCHR_ID": 131, "COUNTRY": "BELIZE", "NAME": "Marco Gonzalez", "POINTSOURC": "NAT. GEOG. LAND OF THE MAYAS", "PROVINCE": 11 }, "geometry": { "type": "Point", "coordinates": [-87.986188183318305, 17.8991024100145] } },
+
+        { "type": "Feature", "properties": { "FID": 352, "AREA": 0, "PERIMETER": 0, "ARCHR_": 302, "ARCHR_ID": 171, "COUNTRY": "BELIZE", "NAME": "Mucnal Tunich", "POINTSOURC": "NAT. GEOG. LAND OF THE MAYAS", "PROVINCE": 12 }, "geometry": { "type": "Point", "coordinates": [-89.100180562537204, 17.011840627360399] } },
+
+        { "type": "Feature", "properties": { "FID": 349, "AREA": 0, "PERIMETER": 0, "ARCHR_": 299, "ARCHR_ID": 159, "COUNTRY": "BELIZE", "NAME": "Pomona", "POINTSOURC": "NAT. GEOG. LAND OF THE MAYAS", "PROVINCE": 15 }, "geometry": { "type": "Point", "coordinates": [-88.396481790526295, 17.011163923975701] } },
+
+        { "type": "Feature", "properties": { "FID": 344, "AREA": 0, "PERIMETER": 0, "ARCHR_": 294, "ARCHR_ID": 172, "COUNTRY": "BELIZE", "NAME": "Tipu", "POINTSOURC": "NAT. GEOG. LAND OF THE MAYAS", "PROVINCE": 12 }, "geometry": { "type": "Point", "coordinates": [-89.0764806474631, 17.045167465212501] } },
+
+        { "type": "Feature", "properties": { "FID": 329, "AREA": 0, "PERIMETER": 0, "ARCHR_": 155, "ARCHR_ID": 150, "COUNTRY": "BELIZE", "NAME": "Colha", "POINTSOURC": "NAT. GEOG. LAND OF THE MAYAS", "PROVINCE": 14 }, "geometry": { "type": "Point", "coordinates": [-88.395425800797199, 17.9757517370284] } },
+
+        { "type": "Feature", "properties": { "FID": 327, "AREA": 0, "PERIMETER": 0, "ARCHR_": 154, "ARCHR_ID": 156, "COUNTRY": "BELIZE", "NAME": "El Pozito", "POINTSOURC": "NAT. GEOG. LAND OF THE MAYAS", "PROVINCE": 14 }, "geometry": { "type": "Point", "coordinates": [-88.640879576140193, 17.986155275633902] } },
+
+        { "type": "Feature", "properties": { "FID": 325, "AREA": 0, "PERIMETER": 0, "ARCHR_": 153, "ARCHR_ID": 149, "COUNTRY": "BELIZE", "NAME": "Kichpanha", "POINTSOURC": "NAT. GEOG. LAND OF THE MAYAS", "PROVINCE": 14 }, "geometry": { "type": "Point", "coordinates": [-88.487727420941894, 17.9876859340213] } },
+
+        { "type": "Feature", "properties": { "FID": 323, "AREA": 0, "PERIMETER": 0, "ARCHR_": 152, "ARCHR_ID": 148, "COUNTRY": "BELIZE", "NAME": "Cuello", "POINTSOURC": "NAT. GEOG. LAND OF THE MAYAS", "PROVINCE": 14 }, "geometry": { "type": "Point", "coordinates": [-88.621805613219905, 18.071477185875398] } },
+
+        { "type": "Feature", "properties": { "FID": 317, "AREA": 0, "PERIMETER": 0, "ARCHR_": 149, "ARCHR_ID": 147, "COUNTRY": "BELIZE", "NAME": "Rio Hondo", "POINTSOURC": "NAT. GEOG. LAND OF THE MAYAS", "PROVINCE": 14 }, "geometry": { "type": "Point", "coordinates": [-88.661889223726604, 18.116805063516502] } },
+
+        { "type": "Feature", "properties": { "FID": 312, "AREA": 0, "PERIMETER": 0, "ARCHR_": 147, "ARCHR_ID": 146, "COUNTRY": "BELIZE", "NAME": "San Estevan", "POINTSOURC": "NAT. GEOG. LAND OF THE MAYAS", "PROVINCE": 14 }, "geometry": { "type": "Point", "coordinates": [-88.497182771227301, 18.133427554511499] } },
+
+        { "type": "Feature", "properties": { "FID": 306, "AREA": 0, "PERIMETER": 0, "ARCHR_": 268, "ARCHR_ID": 173, "COUNTRY": "BELIZE", "NAME": "Cahal Pech", "POINTSOURC": "NAT. GEOG. LAND OF THE MAYAS", "PROVINCE": 12 }, "geometry": { "type": "Point", "coordinates": [-89.081780903427102, 17.088363841059401] } },
+
+        { "type": "Feature", "properties": { "FID": 305, "AREA": 0, "PERIMETER": 0, "ARCHR_": 144, "ARCHR_ID": 143, "COUNTRY": "BELIZE", "NAME": "Shipstern", "POINTSOURC": "NAT. GEOG. LAND OF THE MAYAS", "PROVINCE": 13 }, "geometry": { "type": "Point", "coordinates": [-88.156528601440797, 18.2015141822001] } },
+
+        { "type": "Feature", "properties": { "FID": 303, "AREA": 0, "PERIMETER": 0, "ARCHR_": 143, "ARCHR_ID": 145, "COUNTRY": "BELIZE", "NAME": "Nohmul", "POINTSOURC": "NAT. GEOG. LAND OF THE MAYAS", "PROVINCE": 14 }, "geometry": { "type": "Point", "coordinates": [-88.579374364130999, 18.210500565819402] } },
+
+        { "type": "Feature", "properties": { "FID": 299, "AREA": 0, "PERIMETER": 0, "ARCHR_": 265, "ARCHR_ID": 174, "COUNTRY": "BELIZE", "NAME": "Xunantunich", "POINTSOURC": "NAT. GEOG. LAND OF THE MAYAS", "PROVINCE": 12 }, "geometry": { "type": "Point", "coordinates": [-89.145536368702693, 17.1100009867063] } },
+
+        { "type": "Feature", "properties": { "FID": 293, "AREA": 0, "PERIMETER": 0, "ARCHR_": 138, "ARCHR_ID": 144, "COUNTRY": "BELIZE", "NAME": "Aventura", "POINTSOURC": "NAT. GEOG. LAND OF THE MAYAS", "PROVINCE": 13 }, "geometry": { "type": "Point", "coordinates": [-88.478916245918896, 18.315785680439099] } },
+
+        { "type": "Feature", "properties": { "FID": 289, "AREA": 0, "PERIMETER": 0, "ARCHR_": 136, "ARCHR_ID": 142, "COUNTRY": "BELIZE", "NAME": "Sarteneja", "POINTSOURC": "NAT. GEOG. LAND OF THE MAYAS", "PROVINCE": 13 }, "geometry": { "type": "Point", "coordinates": [-88.160557776243294, 18.324332086985699] } },
+
+        { "type": "Feature", "properties": { "FID": 287, "AREA": 0, "PERIMETER": 0, "ARCHR_": 135, "ARCHR_ID": 141, "COUNTRY": "BELIZE", "NAME": "Cerros", "POINTSOURC": "NAT. GEOG. LAND OF THE MAYAS", "PROVINCE": 13 }, "geometry": { "type": "Point", "coordinates": [-88.358373061605306, 18.333421443574299] } },
+
+        { "type": "Feature", "properties": { "FID": 283, "AREA": 0, "PERIMETER": 0, "ARCHR_": 257, "ARCHR_ID": 175, "COUNTRY": "BELIZE", "NAME": "Buenavista del Cayo", "POINTSOURC": "NAT. GEOG. LAND OF THE MAYAS", "PROVINCE": 12 }, "geometry": { "type": "Point", "coordinates": [-89.118337418855106, 17.147722280856001] } },
+
+        { "type": "Feature", "properties": { "FID": 274, "AREA": 0, "PERIMETER": 0, "ARCHR_": 128, "ARCHR_ID": 140, "COUNTRY": "BELIZE", "NAME": "Santa Maria", "POINTSOURC": "NAT. GEOG. LAND OF THE MAYAS", "PROVINCE": 13 }, "geometry": { "type": "Point", "coordinates": [-88.374942590036099, 18.412862059035401] } },
+
+        { "type": "Feature", "properties": { "FID": 265, "AREA": 0, "PERIMETER": 0, "ARCHR_": 248, "ARCHR_ID": 176, "COUNTRY": "BELIZE", "NAME": "Baking Pot", "POINTSOURC": "NAT. GEOG. LAND OF THE MAYAS", "PROVINCE": 12 }, "geometry": { "type": "Point", "coordinates": [-89.020013370353894, 17.2053827823955] } },
+
+        { "type": "Feature", "properties": { "FID": 250, "AREA": 0, "PERIMETER": 0, "ARCHR_": 242, "ARCHR_ID": 177, "COUNTRY": "BELIZE", "NAME": "Barton Ramie", "POINTSOURC": "NAT. GEOG. LAND OF THE MAYAS", "PROVINCE": 12 }, "geometry": { "type": "Point", "coordinates": [-88.971151957439005, 17.222213263923798] } },
+
+        { "type": "Feature", "properties": { "FID": 234, "AREA": 0, "PERIMETER": 0, "ARCHR_": 234, "ARCHR_ID": 507, "COUNTRY": "BELIZE-GUATE", "NAME": "EL PILAR", "POINTSOURC": "UF-GEOPLAN", "PROVINCE": 26 }, "geometry": { "type": "Point", "coordinates": [-89.152056367204096, 17.243923590678602] } },
+
+
+
+
+
+    ]
+
+};
+
